b7855f890fc5b395715b32dbb1e7ca861e6ca6e6
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-09-20 22:04:32 +0530

    added middlewares

---
+++ apps/backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,20 @@
+import {type Request, type Response, type NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { env } from '../config/env';
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, env.JWT_SECRET) as { id: string };
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
